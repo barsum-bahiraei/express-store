f486c1c6c6cd7fc2dcd30388f076826c60c44316
Amin Bahiraei <amin.bahiraei.75@gmail.com>
2024-02-26 14:45:27 +0330

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from '@prisma/client'
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+const prisma = new PrismaClient()
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
+    prisma.$connect();
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
