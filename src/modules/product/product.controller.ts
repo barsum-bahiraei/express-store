99bf89f8fb3837dce0686c03f14073bbb9f0e163
Amin Bahiraei <amin.bahiraei.75@gmail.com>
2024-02-28 17:25:32 +0330

    first commit

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,8 +1,8 @@
 import {Request, Response} from "express";
 
 export class ProductController {
-    search(req: Request, res: Response) {
-        console.log(req.body)
+    search = (req: Request, res: Response) => {
+        console.log(this)
         res.json(req.body).status(200)
     }
 }
