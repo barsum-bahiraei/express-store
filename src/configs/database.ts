403a46a18dcd4c9474428681ebc6f477b5a946f7
Amin Bahiraei <amin.bahiraei.75@gmail.com>
2024-07-09 15:57:47 +0330

    change

---
+++ src/configs/database.ts
@@ -0,0 +1,18 @@
+import {User} from "../modules/user/entities/user.entity";
+import {DataSource} from "typeorm";
+
+export const appDataSource = new DataSource({
+    type: "mssql",
+    host: "localhost",
+    port: 1433,
+    username: "sa", // Optional for Windows Authentication
+    password: "database1234", // Optional for Windows Authentication
+    database: "TestDatabase",
+    entities: [User],
+    synchronize: true,
+    logging: false,
+    options: {
+        encrypt: false, // Set to true if you're using SSL
+        trustServerCertificate: true, // This enables Windows Authentication
+    }
+})
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express, {Express} from "express";
-import {PrismaClient} from '@prisma/client'
 import dotenv from "dotenv";
 import {baseRoutes} from "./modules/base.routes";
+import {appDataSource} from "./configs/database";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
-const prisma = new PrismaClient()
 
 
 app.use(express.json());
@@ -15,6 +14,10 @@ app.use(express.urlencoded({extended: true}))
 app.use("/api", baseRoutes);
 
 app.listen(port, () => {
-    prisma.$connect();
+    appDataSource.initialize()
+        .then(() => {
+            // here you can start to work with your database
+        })
+        .catch((error) => console.log(error))
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
